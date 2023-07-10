2905ebffc3902c59c7fbfd9e1f91b54b1fa8c903
bethcast <beth.cast06@gmail.com>
2023-07-10 08:05:12 -0600

    initial commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports ={
+
+    ...nextConfig,
+    env:{
+  
+  ZAPATITOS_API_KEY: "AIzaSyBHoH-OzWJo9WtlEOHFBdGB9_M9ICqqoO8",
+  ZAPATITOS_AUTH_DOMAIN: "dsm-3-a1.firebaseapp.com",
+  ZAPATITOS_PROJECT_ID: "dsm-3-a1",
+  ZAPATITOS_STORAGE_BUCKET: "dsm-3-a1.appspot.com",
+  ZAPATITOS_MESSAGING_SENDER_ID: "570940893156",
+  ZAPATITOS_APP_ID: "1:570940893156:web:a77359e03dfc872048c9f1"
+
+
+
+    }
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,14 @@
+import "./login.css"
+export default function login(){
+
+    return (
+
+        <div className="container" >
+
+            <input type="text" placeholder="email"/>
+            <input type="password" placeholder="123456"/>
+            <button>Login</button>
+        </div>
+
+    )
+}
