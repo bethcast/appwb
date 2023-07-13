ddff2afb2e71ad7dea54950c3b63354b3e512335
bethcast <139139023+bethcast@users.noreply.github.com>
2023-07-12 23:14:00 -0600

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,43 @@
+'use client'
+import { useState } from "react";
 import "./login.css"
-export default function login(){
+
+export default function Login() {
+
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
 
     return (
+        <div className="container">
 
-        <div className="container" >
+            <input 
+                onChange={handleChange} 
+                name="email" 
+                value={state.email} 
+                type="text" 
+                placeholder="email" 
+            />
 
-            <input type="text" placeholder="email"/>
-            <input type="password" placeholder="123456"/>
+            <input 
+                name="password" 
+                onChange={handleChange} 
+                type="password" 
+                placeholder="123456" 
+            />
+
             <button>Login</button>
-        </div>
 
+        </div>
     )
+
 }
